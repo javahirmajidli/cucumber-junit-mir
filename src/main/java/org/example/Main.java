4d7fc1b5dbd71c18d063127ef65188ae8fd7df58
Javahir Majidli <cevahirvelizade@gmail.com>
2023-03-04 21:47:46 +0100

    trigger jenkins job

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    //I just want to trigger jenkins job from github therefore I used this project as a demo
 }
